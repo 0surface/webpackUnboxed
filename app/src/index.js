a0bea98241dbb768cc46636cf719183cc5b1cdd5
0surface <tms.0xvoid@gmail.com>
2020-10-28 19:16:30 +0000

    set boostrap up, some attempts to see balances

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,14 @@
 import Web3 from "web3";
 import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import "bootstrap";
 
 const App = {
   web3: null,
   account: null,
+  accounts: [],
   meta: null,
 
-  start: async function() {
+  start: async function () {
     const { web3 } = this;
 
     try {
@@ -15,20 +17,50 @@ const App = {
       const deployedNetwork = metaCoinArtifact.networks[networkId];
       this.meta = new web3.eth.Contract(
         metaCoinArtifact.abi,
-        deployedNetwork.address,
+        deployedNetwork.address
       );
 
       // get accounts
       const accounts = await web3.eth.getAccounts();
+      this.accounts = accounts;
       this.account = accounts[0];
 
       this.refreshBalance();
+      this.showBalance();
     } catch (error) {
       console.error("Could not connect to contract or chain.");
     }
   },
 
-  refreshBalance: async function() {
+  showBalances: async function () {
+    const { getBalance } = this.meta.methods;
+
+    const balance1 = await getBalance(this.accounts[1])
+      .call()
+      .then((balances) => {
+        for (let i = 0; i < 3; i++) {
+          let balanceVal = balances[i].toString();
+          let elem = document.getElementById("address${i}Balance");
+          elem.innerHTML = balanceVal;
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+
+    // console.log("balance1: ", balance1);
+
+    // const addressBalanceElem1 = document.getElementById("address1Balance");
+    // addressBalanceElem1.innerHTML = balance1;
+
+    // for (let i = 0; i < 3; i++) {
+    //   let accBalance = await getBalance(this.accounts[i]).call();
+    //   let balanceElement = document.getElementById("address${i}Balance");
+    //   balanceElement.innerHTML = accBalance;
+    // }
+  },
+
+  refreshBalance: async function () {
     const { getBalance } = this.meta.methods;
     const balance = await getBalance(this.account).call();
 
@@ -36,7 +68,7 @@ const App = {
     balanceElement.innerHTML = balance;
   },
 
-  sendCoin: async function() {
+  sendCoin: async function () {
     const amount = parseInt(document.getElementById("amount").value);
     const receiver = document.getElementById("receiver").value;
 
@@ -47,9 +79,10 @@ const App = {
 
     this.setStatus("Transaction complete!");
     this.refreshBalance();
+    this.showBalances();
   },
 
-  setStatus: function(message) {
+  setStatus: function (message) {
     const status = document.getElementById("status");
     status.innerHTML = message;
   },
@@ -57,18 +90,18 @@ const App = {
 
 window.App = App;
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     window.ethereum.enable(); // get permission to access accounts
   } else {
     console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live"
     );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+      new Web3.providers.HttpProvider("http://127.0.0.1:8545")
     );
   }
 
